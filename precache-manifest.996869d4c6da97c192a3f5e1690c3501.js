1f98270eb6aab450f4920aa31d95997430cca4a9
mroubaud <matiasroubaud@gmail.com>
2023-12-03 14:50:25 +0100

    Updates

---
+++ precache-manifest.996869d4c6da97c192a3f5e1690c3501.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f0eae2d619257c43b61",
+    "url": "/myb/static/css/main.835fa114.chunk.css"
+  },
+  {
+    "revision": "4f0eae2d619257c43b61",
+    "url": "/myb/static/js/main.4f0eae2d.chunk.js"
+  },
+  {
+    "revision": "cdf1f63f2d43c13bf345",
+    "url": "/myb/static/js/1.cdf1f63f.chunk.js"
+  },
+  {
+    "revision": "15e27e8b395ca133547a",
+    "url": "/myb/static/js/runtime~main.15e27e8b.js"
+  },
+  {
+    "revision": "7aa7c004c9015e001008aafc423502c0",
+    "url": "/myb/static/media/MyB_logo-removebg_bw.7aa7c004.png"
+  },
+  {
+    "revision": "04bee9202e52320e77feb789fd187550",
+    "url": "/myb/static/media/hero.04bee920.webp"
+  },
+  {
+    "revision": "1da72f97f3fff9351328448c35c75c89",
+    "url": "/myb/static/media/torre_mecanica_lapeña.1da72f97.png"
+  },
+  {
+    "revision": "1850b5db6d9c75a4bc995bee33824226",
+    "url": "/myb/static/media/hero.1850b5db.jpeg"
+  },
+  {
+    "revision": "e402b995a0c5a7b8491d9180d4803386",
+    "url": "/myb/static/media/san_ignacio_idom.e402b995.png"
+  },
+  {
+    "revision": "8dceaa32630bfd8c6a9c58cbf221d3a8",
+    "url": "/myb/static/media/discesur_ourense_34.8dceaa32.png"
+  },
+  {
+    "revision": "bc06570caa7e80b0b822431729fad6b9",
+    "url": "/myb/static/media/grupo_gmp.bc06570c.png"
+  },
+  {
+    "revision": "42d44da37a13cf6381c0eb50a60dfb46",
+    "url": "/myb/static/media/MyB_logo-removebg.42d44da3.png"
+  },
+  {
+    "revision": "02f1372cacf8e0f2aacdfb8869c8be93",
+    "url": "/myb/static/media/cta_contact_resize.02f1372c.jpeg"
+  },
+  {
+    "revision": "2145bd3ddbfadc7e09f3ba086add4f61",
+    "url": "/myb/index.html"
+  }
+];
